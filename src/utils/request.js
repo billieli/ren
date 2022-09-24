cbd0680ddf4c1876f4b4027b775c8eb3c152d472
billieli <billie.li.7@outlook.com>
2022-09-24 10:49:21 +0800

    feat:端口标题修改

--- src/utils/request.js
+++ src/utils/request.js
@@ -2,7 +2,7 @@
 import axios from 'axios'
 // 创建axios实例
 const service = axios.create({
-  baseURL: 'devic',
+  baseURL: process.env.VUE_APP_BASE_API,
   timeout: 5000
 })
 export default service
--- vue.config.js
+++ vue.config.js
@@ -14,7 +14,7 @@ const name = defaultSettings.title || 'vue Admin Template' // page title
 // You can change the port by the following methods:
 // port = 9528 npm run dev OR npm run dev --port = 9528
 const port = process.env.port || process.env.npm_config_port || 9528 // dev port
-
+// console.log(process.env)
 // All configuration item explanations can be find in https://cli.vuejs.org/config/
 module.exports = {
   /**
@@ -38,6 +38,7 @@ module.exports = {
     }
     // before: require('./mock/mock-server.js')
   },
+
   configureWebpack: {
     // provide the app's title in webpack's name field, so that
     // it can be accessed in index.html to inject the correct title.
