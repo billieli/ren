7305c4d54b1e1856031f3c15bbe9fb6fba246fe9
billieli <billie.li.7@outlook.com>
2022-09-26 09:25:34 +0800

    feat:login完成

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,8 +1,21 @@
 // axios封装
 import axios from 'axios'
+import { Message } from 'element-ui'
 // 创建axios实例
 const service = axios.create({
   baseURL: process.env.VUE_APP_BASE_API,
   timeout: 5000
 })
+service.interceptors.response.use((res) => {
+  const { message, data, success } = res.data
+  if (success) {
+    return data
+  }
+  Message.error(message)
+  return Promise.reject(new Error(message))
+},
+error => {
+  return Promise.reject(error)
+}
+)
 export default service
