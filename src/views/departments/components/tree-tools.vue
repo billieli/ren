56f936f577e3511035849a83608a960f3ef40188
billieli <billie.li.7@outlook.com>
2022-09-30 13:46:47 +0800

    feat:组织架构，新增校验完成

---
+++ src/views/departments/components/tree-tools.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <el-row type="flex" justify="space-around" align="middle" style="height:40px;width:100%;">
+      <el-col>
+        <span>{{ treeNode.name }}</span>
+      </el-col>
+      <el-col :span="4">
+        <el-row type="flex">
+          <el-col>
+            <span>{{ treeNode.manager }}</span>
+          </el-col>
+          <el-col>
+            <el-dropdown @command="handleCommand">
+              <span>
+                操作<i class="el-icon-arrow-down el-icon--right" />
+              </span>
+              <el-dropdown-menu slot="dropdown">
+                <el-dropdown-item command="add">添加子部门</el-dropdown-item>
+                <el-dropdown-item v-if="isrool" command="edit">编辑子部门</el-dropdown-item>
+                <el-dropdown-item v-if="isrool" command="del">删除子部门</el-dropdown-item>
+
+              </el-dropdown-menu>
+            </el-dropdown>
+          </el-col>
+        </el-row>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    treeNode: {
+      type: Object,
+      required: true
+    },
+    isrool: {
+      type: Boolean,
+      default: true
+    }
+  },
+  methods: {
+    handleCommand(type) {
+      if (type === 'add') {
+        // 新增
+        console.log('add')
+
+        this.$emit('addDepts', this.treeNode)
+      } else if (type === 'edit') {
+        console.log('edit')
+        // 编辑
+      } else {
+        // 删除
+        console.log('del')
+      }
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/views/departments/index.vue
+++ src/views/departments/index.vue
@@ -1,29 +1,69 @@
 <template>
   <div>
-    departments
+    <div class="departments-container">
+      <el-card>
+        <tree-node :tree-node="company" :isrool="false" @addDepts="handleAddDept" />
+      </el-card>
+      <el-tree :data="departs" :default-expand-all="true" :props="defaultProps">
+        <treeNode slot-scope="{data}" :tree-node="data" style="width:100%" @addDepts="handleAddDept" />
+      </el-tree>
+    </div>
+    <add-dept :show-dialog.sync="showDialog" :tree-node="currentNode" />
   </div>
 </template>
 
 <script>
+import { getDepartments } from '@/api/departments'
+import treeNode from './components/tree-tools.vue'
+import { tranListToTreeData } from '@/utils/index'
+import AddDept from './components/add-dept.vue'
 export default {
   name: 'HrsaasIndex',
+  components: { treeNode, AddDept },
 
   data() {
     return {
-
+      defaultProps: {
+        label: 'name'
+      },
+      departs: [{ name: '总裁办', manager: '曹操', children: [{ name: '董事会', manager: '曹丕' }] },
+        { name: '行政部', manager: '刘备' },
+        { name: '人事部', manager: '孙权' }],
+      company: { name: '江苏传智播客教育科技股份有限公司', manager: '负责人' },
+      showDialog: false,
+      currentNode: {}
     }
   },
 
   mounted() {
-
+    this.getDepartments()
   },
 
   methods: {
-
+    async getDepartments() {
+      const { depts, companyManage, companyName } = await getDepartments()
+      console.log(depts)
+      this.departs = tranListToTreeData(depts, '')
+      this.company = { name: companyName, manager: companyManage, id: '' }
+    },
+    handleAddDept(node) {
+      console.log(111)
+      this.showDialog = true
+      this.currentNode = node
+    }
   }
 }
 </script>
 
 <style lang="scss" scoped>
 
+.departments-container {
+  width: 900px;
+  margin: 20px auto;
+  .el-tree {
+    .el-tree-node__content {
+      padding-right: 0px;
+    }
+  }
+}
 </style>
